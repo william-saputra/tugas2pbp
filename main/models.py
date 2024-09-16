7bc15d31548ce035b663883dbce7468115087555
william-saputra <williammatthews.id@gmial.com>
2024-09-16 21:00:49 +0700

    final

--- main/models.py
+++ main/models.py
@@ -1,12 +1,19 @@
 from django.db import models
+# from django.core.validators import MinValueValidator, MaxValueValidator
+import uuid
+
 
 # Create your models here.
 class Product(models.Model): #Lebih cocok namanya product, leather store utk nama project django nya (tugas2pbp)....
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=70)
     price = models.IntegerField()
     description = models.TextField(max_length=2000)
     thickness = models.DecimalField(max_digits=4, decimal_places=2)
     user_reviews = models.TextField(max_length=1000)
+    user_ratings = models.IntegerField()
+
+    
 
     # @property
     # def is_mood_strong(self):
--- main/views.py
+++ main/views.py
@@ -1,21 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from main.forms import ProductForm
+from main.models import Product
+from django.http import HttpResponse
+from django.core import serializers
+
+
 
 # Create your views here.
 def show_main(request):
+    product_entries = Product.objects.all()
     context = {
         'app' : 'Leather Store',
         'name': 'William Matthew Saputra',
-        'class': 'PBP F',
-        'product': 'New Ontario Synthetic Leather',
-        'price': 75000,
-        'thickness': 1.0,
-        'description': 'Ontario memiliki ukuran 1,37 meter x 1 meter dengan ketebalan 0,5 mm.\n'
-                        'Beratnya mencapai 350 gram per meter, terbuat dari bahan PVC yang kuat dan tahan lama.\n'
-                        'Selain itu, produk ini dilengkapi dengan backing yang berbahan knitted, memberikan dukungan ekstra untuk keawetan dan kenyamanan penggunaannya.',
-        'user_reviews': 'Bagus bgt packing bener2 aman bgt Recomend banget nih toko nya ayo guyss buruan order ditoko ini.. Terimakasih.',
-
-
-        
+        'class': 'PBP F',   
+        'product_entries' : product_entries   
     }
 
     return render(request, "main.html", context)
+
+def create_product_entry(request):
+    form = ProductForm(request.POST or None)
+
+    if form.is_valid() and request.method == "POST":
+        form.save()
+        return redirect('main:show_main')
+
+    context = {'form': form}
+    return render(request, "create_product_entry.html", context)
+
+def show_xml(request):
+    data = Product.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request):
+    data = Product.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_xml_by_id(request, id):
+    data = Product.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json_by_id(request, id):
+    data = Product.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
